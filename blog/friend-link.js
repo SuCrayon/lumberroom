ca98671b319b68070ae149a75d955000fa77dd42
Crayon <614820984@qq.com>
2022-09-01 13:05:06 +0800

    [UPDATE] 友链文件改用js文件

---
+++ blog/friend-link.js
@@ -0,0 +1,20 @@
+/*
+ * @Author: Crayon
+ * @Date: 2022-09-01 12:30:51
+ * @Last Modified by: Crayon
+ * @LastEditTime: 2022-09-01 12:36:49
+ */
+export const friendLinkList = [
+    {
+        "class_name": "友情链接",
+        "class_desc": "那些人，那些事",
+        "link_list": [
+            {
+                "name": "ZhengwenFeng's blog",
+                "link": "https://tenqaz.github.io/",
+                "avatar": "https://tenqaz.github.io/img/me.jpg",
+                "descr": ""
+            }
+        ]
+    }
+]
